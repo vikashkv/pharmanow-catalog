4d9cdfe2aae3a5faa4b1ae9f6f561e68bd7c8b74
Vikash Vishwakarma <vikashkv1995@gmail.com>
2025-08-14 12:44:32 +0530

    first commit

---
+++ src/main/java/com/pharmanow/catalog/PharmanowCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.pharmanow.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmanowCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmanowCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmanow/catalog/PharmanowCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmanow.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmanowCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
